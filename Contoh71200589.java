717237c424ca88e065b070e708bf959879167175
ivan <ivan.reynaldo@ti.ukdw.ac.id>
2022-02-08 15:54:55 +0700

    commit pertama

---
+++ Contoh71200589.java
@@ -0,0 +1,17 @@
+public class Contoh71200589 {
+        public static void main(String args[]) {
+                int x=10;
+                int y=25;
+                int z=x+y;
+                
+                System.out.printIn("Hasil penjumlahan"
+                        +" dari x+y = " + z);
+                System.out.printIn("Hasil akar kuadrat "
+                        +"dari z =" + Math.sqrt(z));
+                
+                String namadepan = "Antonius";
+                System.out.printIn("Nama depan saya adalah =" + namadepan);
+                System.out.printIn("Nama lengkap saya "
+                        +"adalah = " + namadepan + " Rachmat");
+                }
+}
